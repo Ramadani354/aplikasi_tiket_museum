318bea30f01d762e5d2fb88fb9dc7ace6d8159e9
yoviadhi123@gmail.com <yoviadhi123@gmail.com>
2023-05-12 15:36:19 +0700

    fix ticket.go

--- app/models/ticket.go
+++ app/models/ticket.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Ticket struct {
 	gorm.Model
 	ID      uint `json:"id"`
-	IDAdmin uint `gorm:"column:id_admin" json:"id_admin"`
+	IDAdmin uint `gorm:"column:id_admin" json:"-"`
 	Harga   uint `json:"harga"`
 	Kuota   uint `json:"kuota"`
 }
